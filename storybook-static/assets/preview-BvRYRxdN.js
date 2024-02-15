b337252fec59cf2da156ee0e739cb0c5dffb880d
TMG23 <sub@takwene.com>
2024-02-15 04:12:53 +0200

    first commit

---
+++ storybook-static/assets/preview-BvRYRxdN.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-CnhMP_v6.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-BKF9siqY.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-BKF9siqY.js","./iframe-CnhMP_v6.js","./index-DH5ua8nC.js","./_commonjsHelpers-Cpj98o6Y.js","./react-18-iZ4baTDS.js","./index-D9uilScg.js","./index-4clEai0d.js","./index-Bw8VTzHM.js","./index-UvUotFBn.js","./index-DrFu-skq.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
